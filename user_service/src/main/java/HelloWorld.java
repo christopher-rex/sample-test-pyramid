6aa1983b6cefec69575c5f520cca1342ce8b430b
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-06 22:37:01 +0800

    [user_service] Initial commit
    - Added Spark for REST APIs
    - Gradle 3.4 does not work well with IntelliJ

---
+++ user_service/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+        get("/ping", (req, res) -> "Pong");
+    }
+}

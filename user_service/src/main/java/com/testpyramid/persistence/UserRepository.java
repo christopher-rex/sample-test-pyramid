bf0d0a12ada8b2789b1bf31a48d76a47067ed13d
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-08 17:12:53 +0800

    [user_service] Make database name configurable
    - Environment + file based configuration in Java is such a mess

--- user_service/src/main/java/com/testpyramid/persistence/UserRepository.java
+++ user_service/src/main/java/com/testpyramid/persistence/UserRepository.java
@@ -1,12 +1,17 @@
 package com.testpyramid.persistence;
 
+import com.gojek.ApplicationConfiguration;
+import com.gojek.Figaro;
 import org.skife.jdbi.v2.DBI;
 
 import java.util.Map;
 
 public class UserRepository {
     public Map<String, String> findByEmailAndPassword(String email, String password) {
-        DBI dbi = new DBI("jdbc:sqlite:user_service.db");
+        ApplicationConfiguration config = Figaro.configure(null);
+        DBI dbi = new DBI("jdbc:sqlite:" +
+                getClass().getClassLoader().getResource(
+                        config.getValueAsString("DB_NAME") + ".db"));
         UserDao dao = dbi.open(UserDao.class);
 
         return dao.findByEmailAndPassword(email, password);

0d97eac64b792f2caf6aeea5cc112163a4374ee1
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-09 17:08:24 +0800

    [fronted] Fix validation message logic
    - Messages need to be cleared once an invalid element has been fixed

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,9 @@
 document.querySelector('#login-form').addEventListener('submit', event => {
 	event.preventDefault()
 
+	document.querySelectorAll('.validation-message').forEach(element =>
+		element.innerText = '')
+
 	const [email, password] =
 		[].map.call(document.querySelectorAll('input'), node => node.value)
 
@@ -11,6 +14,15 @@ document.querySelector('#login-form').addEventListener('submit', event => {
 		if (response.status === 200) notify('success', 'Logged in successfully.')
 		else if (response.status === 401) notify('error', 'You are not authorized.')
 		else notify('error', 'A surprising error occurred.')
+	})
+})
+
+document.querySelectorAll('input').forEach(element => {
+	element.addEventListener('change', event => {
+		const isValid = event.target.checkValidity()
+		if (isValid) {
+			event.target.parentNode.querySelector('.validation-message').innerText = ''
+		}
 	})
 })
 

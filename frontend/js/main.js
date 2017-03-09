8b503890a80d1376fb9befd9b9d24279a7cd6f97
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-09 17:30:27 +0800

    [frontend] Add ad-hoc templating and routing

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,37 +1,54 @@
-document.querySelector('#login-form').addEventListener('submit', event => {
-	event.preventDefault()
+function route() {
+	const defaultRoute = 'login'
+	const currentRoute = location.hash.substring(1) || defaultRoute
 
-	document.querySelectorAll('.validation-message').forEach(element =>
-		element.innerText = '')
+	const templateNode = document.querySelector(`#${currentRoute}-template`)
 
-	const [email, password] =
-		[].map.call(document.querySelectorAll('input'), node => node.value)
+	if (!templateNode) return
 
-	fetch('http://localhost:4567/login', {
-		method: 'post',
-		body: JSON.stringify({email, password})
-	}).then(response => {
-		if (response.status === 200) notify('success', 'Logged in successfully.')
-		else if (response.status === 401) notify('error', 'You are not authorized.')
-		else notify('error', 'A surprising error occurred.')
-	})
-})
+	document.querySelector('#output').innerHTML = templateNode.innerHTML
+	init[currentRoute]()
+	// TODO cleanup event listeners
+}
 
-document.querySelectorAll('input').forEach(element => {
-	element.addEventListener('change', event => {
-		const isValid = event.target.checkValidity()
-		if (isValid) {
-			event.target.parentNode.querySelector('.validation-message').innerText = ''
-		}
-	})
-})
+const init = {
+	login() {
+		document.querySelector('#login-form').addEventListener('submit', event => {
+			event.preventDefault()
 
-document.addEventListener('invalid', event => {
-	event.preventDefault()
+			document.querySelectorAll('.validation-message').forEach(element =>
+				element.innerText = '')
 
-	event.target.parentNode.querySelector('.validation-message').innerText =
-		event.target.validationMessage
-}, true)
+			const [email, password] =
+				[].map.call(document.querySelectorAll('input'), node => node.value)
+
+			fetch('http://localhost:4567/login', {
+				method: 'post',
+				body: JSON.stringify({email, password})
+			}).then(response => {
+				if (response.status === 200) notify('success', 'Logged in successfully.')
+				else if (response.status === 401) notify('error', 'You are not authorized.')
+				else notify('error', 'A surprising error occurred.')
+			})
+		})
+
+		document.querySelectorAll('input').forEach(element => {
+			element.addEventListener('change', event => {
+				const isValid = event.target.checkValidity()
+				if (isValid) {
+					event.target.parentNode.querySelector('.validation-message').innerText = ''
+				}
+			})
+		})
+
+		document.addEventListener('invalid', event => {
+			event.preventDefault()
+
+			event.target.parentNode.querySelector('.validation-message').innerText =
+				event.target.validationMessage
+		}, true)
+	}
+}
 
 function notify(severity, message) {
 	const notice = document.querySelector('#notice')
@@ -41,3 +58,5 @@ function notify(severity, message) {
 	notice.classList.remove('hidden')
 }
 
+window.addEventListener('hashchange', route)
+route()

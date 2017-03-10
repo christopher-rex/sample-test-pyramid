2d9313235ac90eb54b8bea0a59ad526452e7af93
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-10 15:29:38 +0800

    [user_service] Add support for functional tests

--- user_service/src/test/java/UserServiceTest.java
+++ user_service/src/test/java/UserServiceTest.java
@@ -1,5 +1,34 @@
-import static org.junit.Assert.*;
+import com.despegar.http.client.GetMethod;
+import com.despegar.http.client.HttpResponse;
+import com.despegar.sparkjava.test.SparkServer;
+import com.testpyramid.UserService;
+import org.junit.ClassRule;
+import org.junit.Test;
+import spark.servlet.SparkApplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UserServiceTest {
+    public static class UserServiceTestSparkApplication implements SparkApplication {
+        @Override
+        public void init() {
+            UserService.main(new String[]{});
+        }
+    }
 
+    @ClassRule
+    public static SparkServer<UserServiceTestSparkApplication> testServer =
+            new SparkServer<>(UserServiceTestSparkApplication.class, 4568);
+
+    @Test
+    public void testPing() throws Exception {
+        GetMethod get = testServer.get("/ping", false);
+
+        HttpResponse httpResponse = testServer.execute(get);
+
+        assertEquals(200, httpResponse.code());
+        assertEquals("pong", new String(httpResponse.body()));
+        assertNotNull(testServer.getApplication());
+    }
 }

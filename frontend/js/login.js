202915c5668caaa41286dc9bbf9cb501ea99a43e
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-10 17:57:50 +0800

    [frontend] Break them long lines

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -17,7 +17,9 @@ const login = (_ => {
 					notify('success', 'Logged in successfully.')
 					location.hash = 'coupon'
 				}
-				else if (response.status === 401) notify('error', 'You are not authorized.')
+				else if (response.status === 401) {
+					notify('error', 'You are not authorized.')
+				}
 				else notify('error', 'A surprising error occurred.')
 			})
 		},
@@ -39,7 +41,8 @@ const login = (_ => {
 
 	return {
 		init() {
-			document.querySelector('#login-form').addEventListener('submit', listeners.handleSubmit)
+			document.querySelector('#login-form').addEventListener('submit',
+				listeners.handleSubmit)
 
 			document.querySelectorAll('input').forEach(element => {
 				element.addEventListener('change', listeners.handleInputChange)
@@ -49,7 +52,8 @@ const login = (_ => {
 		},
 
 		cleanup() {
-			document.querySelector('#login-form').removeEventListener('submit', listeners.handleSubmit)
+			document.querySelector('#login-form').removeEventListener('submit',
+				listeners.handleSubmit)
 
 			document.querySelectorAll('input').forEach(element => {
 				element.removeEventListener('change', listeners.handleInputChange)

c467f75b652786404bca3f26ff7d68d5255f2f9b
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-03-27 23:29:04 +0800

    [frontend] Handle `warning` class in notifier
    - Needs to be cleaned up since it's a valid notification type

--- frontend/js/notifier.js
+++ frontend/js/notifier.js
@@ -1,7 +1,7 @@
 const notifier = {
 	init(notifierElement) {
 		return (severity, message) => {
-			notifierElement.classList.remove('success', 'error')
+			notifierElement.classList.remove('success', 'error', 'warning')
 			notifierElement.innerText = message
 			notifierElement.classList.add(severity)
 			notifierElement.classList.remove('hidden')
